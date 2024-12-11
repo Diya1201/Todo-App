d46bcbe040cbeb92694b7d2eafb92e73bc6d3539
Diya1201 <sanghvidiya@gmail.com>
2024-12-11 02:13:21 -0500

    commit

--- Frontend/todo/src/Important.jsx
+++ Frontend/todo/src/Important.jsx
@@ -26,7 +26,7 @@ const Important = () => {
     const refreshTasks = async () => {
         setLoading(true);
         try {
-            const response = await fetch("http://localhost:5000/api/tasks");
+            const response = await fetch("http://todo-app-u4re.vercel.app/api/tasks");
             const data = await response.json();
             const importantTasks = data.filter((task) => task.isStarred);
             setTasks(importantTasks);
