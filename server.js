062ef51c1bd986f44b969a93094f8e3a103ac37d
Diya1201 <sanghvidiya@gmail.com>
2024-12-12 00:31:56 -0500

    commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Import the tasks router
+const tasksRouter = require('./routes/tasks'); // Adjust the path to your tasks.js file
+
+const app = express();
+const port = process.env.PORT || 5000; // Use dynamic port for Heroku
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON requests
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log('MongoDB connection error: ', err));
+
+// Use the tasks router for the /api/tasks route
+app.use('/api/tasks', tasksRouter);
+
+// Default Route
+app.get('/', (req, res) => {
+  res.json("Hello");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
